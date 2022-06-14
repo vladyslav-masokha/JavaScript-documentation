1a44a8cc6b1641d7851430e2e6b6f10b7dcdbf14
Vladyslav <animew1243@gmail.com>
2022-06-14 07:56:58 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -9,3 +9,36 @@ document.getElementById('check-burger').onclick = () => {
 	navEl[0].classList.remove('un-active-navbar');
 	navEl[0].style.display = 'flex';
 };
+
+// ! Scroll - TODO: not working
+// const anchors = document.querySelectorAll('a[href="#"');
+
+// for (let anchor of anchors) {
+// 	anchor.addEventListener('click', e => {
+// 		e.preventDefault();
+// 		const blockID = anchor.getAttribute('href');
+// 		document.querySelector('' + blockID).scrollIntoView({
+// 			behavior: 'smooth',
+// 			block: 'start',
+// 		});
+// 	});
+// }
+
+// document.querySelectorAll('a[href="#"').forEach(link => {
+// 	link.addEventListener('click', function (e) {
+// 		e.preventDefault();
+
+// 		let href = this.getAttribute('href');
+
+// 		const scrollTarget = document.getElementById(href);
+
+// 		const topOfSet = document.querySelector('.header').offsetHeight;
+// 		const elementPosition = scrollTarget.getBoundingClientRect().top;
+// 		const offsetPosition = elementPosition - topOfSet;
+
+// 		window.scrollBy({
+// 			top: offsetPosition,
+// 			behavior: 'smooth',
+// 		});
+// 	});
+// });
