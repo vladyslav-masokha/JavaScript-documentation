535ed78b3fa680b9c3fdac33aa07c142b8f90046
Vladyslav <animew1243@gmail.com>
2022-06-14 07:26:03 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-'use strict';
-// TODO: Navigation
-$('.close').click(function () {
-	$('.nav').addClass('un-active-navbar');
-});
+// Navigation
+const navEl = document.getElementsByTagName('nav');
 
-$('.check-burger').click(function () {
-	$('.nav').removeClass('un-active-navbar');
-});
+document.getElementById('close-menu').onclick = () => {
+	navEl[0].classList.add('un-active-navbar');
+	navEl[0].style.display = 'none';
+};
+document.getElementById('check-burger').onclick = () => {
+	navEl[0].classList.remove('un-active-navbar');
+	navEl[0].style.display = 'flex';
+};
